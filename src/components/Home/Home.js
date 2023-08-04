08d3d8de3716b932b9787a9fcfc773e0dcd31966
T <tianmingchu@gmail.com>
2023-08-03 20:54:08 -0700

    home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,58 +3,84 @@ import Particle from "../Particle"
 import Typewriter from "typewriter-effect"
 import { Fade } from "react-reveal"
 import { Container } from "react-bootstrap"
-import { Row, Col } from "react-bootstrap"
-import { SocialIcon } from "react-social-icons"
 import ethLogo from "../../assets/home/ethLogo.gif"
+import { FaTwitter, FaGithub, FaGooglePlusG, FaInstagram, FaLinkedin } from "react-icons/fa"
 import "./Home.css"
 
 const Home = () => {
   return (
-    <Container fluid="lg" className="home-container">
+    <Container className="home-container">
       <Particle />
-      <Fade bottom duration={1000} distance="20px">
-        <Row>
-          <Col md={6}>
-            <h1 className="text-center mb-5 header-1">Hi<span className='exclaim'>!</span></h1>
-            <h2 className="header-2">I'm <strong className="pink">Tianming Chu</strong></h2>
-            {/* My passion is developing software that improves the quality of life of others */}
-            <Typewriter 
-              options={{
-                strings: [
-                  "Developer",
-                  "Lifelong Learner"
-                ],
-                autoStart: true,
-                loop: true,
-                deleteSpeed: 50,
-                pauseFor: 2000,
-              }} 
-            />
-            {/* <div className="social-container"> */}
-            <Row className="social-container d-flex text-center">
-              <Col xs={4} md={2}>
-                <SocialIcon fgColor="whitesmoke" target="_blank" rel="noopener noreferrer" className="social" url="https://twitter.com/ti4nming" />
-              </Col>
-              <Col xs={4} md={2}>
-                <SocialIcon fgColor="whitesmoke" target="_blank" rel="noopener noreferrer" className="social" url="mailto:tianmingchu@gmail.com" network="google" />
-              </Col>
-              <Col xs={4} md={2}>
-                <SocialIcon fgColor="whitesmoke" target="_blank" rel="noopener noreferrer" className="social" url="https://github.com/tianmingchu" />
-              </Col>
-              <Col xs={4} md={2}>
-                <SocialIcon fgColor="whitesmoke" target="_blank" rel="noopener noreferrer" className="social" url="https://instagram.com/_tianming/" />
-              </Col>
-              <Col xs={4} md={2}>
-                <SocialIcon fgColor="whitesmoke" target="_blank" rel="noopener noreferrer" className="social" url="https://linkedin.com/in/tianming-chu" />
-              </Col>
-              <Col xs={4} md={2} />
-            </Row>
-            {/* </div> */}
-          </Col>
-          <Col md={6} className="d-flex justify-content-center">
-            <img className="img-fluid mt-5" src={ethLogo} />
-          </Col>
-        </Row>
+      <Fade left duration={1000} distance="20px">
+        <div className="home-wrapper">
+          <div className='d-flex justify-content-center'>
+            <img className="img-fluid eth-logo" src={ethLogo} alt="eth" />
+          </div>
+          <h2 className="name"><strong>Tianming Chu</strong></h2>
+          <Typewriter 
+            options={{
+              strings: [
+                "Developer",
+                "Lifelong Learner"
+              ],
+              autoStart: true,
+              loop: true,
+              deleteSpeed: 50,
+              pauseFor: 2000,
+            }} 
+          />
+        </div>
+      </Fade>
+      <Fade right duration={1500} distance="25px">
+        <div className="social-wrapper">
+          <ul>
+            <li>
+              <a className="github" href="https://github.com/tianmingchu" target="_blank" rel="noopener noreferrer">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <FaGithub style={{marginBottom: "4px"}} />
+              </a>
+            </li>
+            <li>
+              <a className="twitter" href="https://twitter.com/ti4nming" target="_blank" rel="noopener noreferrer">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <FaTwitter style={{marginBottom: "4px"}} />
+              </a>
+            </li>
+            <li>
+              <a className="google" href="mailto:tianmingchu@gmail.com" target="_blank" rel="noopener noreferrer">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <FaGooglePlusG style={{marginBottom: "4px", transform: "scale(1.2)"}} />
+              </a>
+            </li>
+            <li>
+              <a className="instagram" href="https://instagram.com/_tianming/" target="_blank" rel="noopener noreferrer">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <FaInstagram style={{marginBottom: "4px"}} />
+              </a>
+            </li>
+            <li>
+              <a className="linkedin" href="https://linkedin.com/in/tianming-chu" target="_blank" rel="noopener noreferrer">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <FaLinkedin style={{marginBottom: "4px"}} />
+              </a>
+            </li>
+          </ul>
+        </div>
       </Fade>
     </Container>
   )
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -2,26 +2,30 @@ import React from 'react'
 import Particle from "../Particle"
 import { Container } from "react-bootstrap"
 import { Fade } from "react-reveal"
+import Footer from "../Footer/Footer"
 import ResumePDF from "../../assets/resume/resume.pdf"
 import "./Resume.css"
 
 const Resume = () => {
   var width = window.innerWidth;
   return (
-    <Container className="resume-container">
-      <Particle />
-      <Fade bottom duration={1200} distance="25px">
-        {(width > 767) ? <object data={ResumePDF} type="application/pdf" width="100%" height="780vh">
-          <div className="d-flex text-center mt-5">
-            <p>Your web browser doesn't have a PDF plugin. Instead you can&nbsp;<a href={ResumePDF} target="_blank" rel="noopener noreferrer">click here to download the PDF.</a></p>
-          </div>
-        </object> : <object width="100%" height="100%" data="https://docs.google.com/gview?embedded=true&url=https://tianming.dev/static/media/resume.358312ae9ad172410b17.pdf">
-          <div className="d-flex text-center mt-5">
-            <p>Your web browser doesn't have a PDF plugin. Instead you can&nbsp;<a href={ResumePDF} target="_blank" rel="noopener noreferrer">click here to download the PDF.</a></p>
-          </div>
-        </object>}
-      </Fade>
-    </Container>
+    <>
+      <Container className="resume-container mb-3">
+        <Particle />
+        <Fade bottom duration={1200} distance="25px">
+          {(width > 767) ? <object data={ResumePDF} type="application/pdf" width="100%" height="780vh">
+            <div className="d-flex text-center mt-5">
+              <p>Your web browser doesn't have a PDF plugin. Instead you can&nbsp;<a href={ResumePDF} target="_blank" rel="noopener noreferrer">click here to download the PDF.</a></p>
+            </div>
+          </object> : <object width="100%" height="100%" data="https://docs.google.com/gview?embedded=true&url=https://tianming.dev/static/media/resume.358312ae9ad172410b17.pdf">
+            <div className="d-flex text-center mt-5">
+              <p>Your web browser doesn't have a PDF plugin. Instead you can&nbsp;<a href={ResumePDF} target="_blank" rel="noopener noreferrer">click here to download the PDF.</a></p>
+            </div>
+          </object>}
+        </Fade>
+      </Container>
+      <Footer />
+    </>
   )
 }
 
