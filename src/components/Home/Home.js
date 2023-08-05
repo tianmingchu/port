8bcf6be974e930e62c7daaba43a47caddbc34a69
T <tianmingchu@gmail.com>
2023-08-05 14:49:45 -0700

    x

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -52,7 +52,7 @@ const Home = () => {
                 {/* <FaTwitter className="social-icon" /> */}
                 <svg version="1.1" id="svg5" xmlnsSvg="http://www.w3.org/2000/svg"
                   xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1668.56 1221.19"
-                  style={{enableBackground:"new 0 0 1668.56 1221.19;", transform:"scale(0.75)", marginBottom:"5px"}} xmlSpace="preserve">
+                  style={{enableBackground:"new 0 0 1668.56 1221.19;", transform:"scale(0.75)"}} className="x-wrapper" xmlSpace="preserve">
                 <g id="layer1" transform="translate(52.390088,-25.058597)">
                   <path className="xxx" fill="#9dc0e4" id="path1009" d="M283.94,167.31l386.39,516.64L281.5,1104h87.51l340.42-367.76L984.48,1104h297.8L874.15,558.3l361.92-390.99
                     h-87.51l-313.51,338.7l-253.31-338.7H283.94z M412.63,231.77h136.81l604.13,807.76h-136.81L412.63,231.77z"/>
